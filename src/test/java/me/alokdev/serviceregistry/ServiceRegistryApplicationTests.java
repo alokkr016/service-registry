b06c71b0617c06fe4168186b4ffb7ec53e0b0596
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2023-07-28 01:38:43 +0530

    Created a eureka server

---
+++ src/test/java/me/alokdev/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alokdev.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
